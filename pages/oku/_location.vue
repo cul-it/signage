6e523ed034e2d4c1b9e0f2fb6ebe89fdc76dde56
Nick Cappadona <nickypasta@gmail.com>
2017-05-25 11:00:56 -0400

    Layout and dynamic route for OKU circ
    Demonstrates:
    * Introducing styles at layout level
    
    * Passing URL params to the page component via dynamic routing

---
+++ pages/oku/_location.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="oku-circ">
+    <h1>{{ location }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function data () {
+    return {
+      location: this.$route.params.location
+    }
+  },
+  layout: 'oku-circ'
+}
+</script>
+
+<style>
+</style>
