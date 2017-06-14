b87e8cd2e45ee0f6c04643a105678a5160cf4f04
Manolo Bevia <manolo.bevia@cornell.edu>
2017-06-14 12:11:21 -0400

    Remove out of the box next homepage content and add placeholder for CU Library Signage homepage

---
+++ components/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <header class="dashboard">
+      <h1 class="dashboard__header">Cornell University Library Signage</h1>
+    </header>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
