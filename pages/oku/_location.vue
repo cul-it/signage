3b353f82ad11b6d9338c7b2b7e1e5c5fcf1524b3
Nick Cappadona <nickypasta@gmail.com>
2017-08-08 12:43:54 -0400

    Sync current time every 10s
    Would have preferred to use setInterval in the store's action directly,
    but encountered a bug where the action was unsuccessful in committing a
    mutation called within setInterval. This is not reproducible in vanilla
    Vue, and appears to be isolated to Nuxt. I have some demos in glitch
    which I will link in the PR.
    
    Add styles borrowed from Mann's circdisplay for blinking colon.
    
    Also, hat tip to Jonnie Hallman for the great post on reactive time with
    Vue.js [1].
    
    [1] https://cushionapp.com/journal/reactive-time-with-vuejs

--- pages/oku/_location.vue
+++ pages/oku/_location.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="oku-circ" v-bind:class="okuLocation">
-    <time>{{ currentTime }}</time>
+    <time v-html="currentTime"></time>
 
     <oku-circ location="olin"/>
     <oku-circ location="uris"/>
@@ -10,16 +10,21 @@
 <script>
 import OKU from '~components/oku-circ'
 import moment from 'moment'
+import { mapState } from 'vuex'
 
 export default {
   data () {
     return {
-      okuLocation: this.$route.params.location,
-      currentTime: moment().format('MMM D / h:mm A')
+      okuLocation: this.$route.params.location
     }
   },
   components: {
     'oku-circ': OKU
+  },
+  computed: {
+    ...mapState({
+      currentTime: state => moment(state.time.now).format('MMM D / h[<span class="blink">:</span>]mm A')
+    })
   },
   async fetch ({ store, params }) {
     await store.dispatch('laptops/fetchStatus', 'olin')
@@ -27,9 +32,28 @@ export default {
     await store.dispatch('laptops/fetchStatus', 'uris')
     await store.dispatch('phoneChargers/fetchStatus', 'uris')
   },
-  layout: 'oku'
+  layout: 'oku',
+  mounted () {
+    // Sync current time every 10 seconds
+    // -- ideally, this would happen within the store itself (as part of the action),
+    // -- but encountered a bug when attempting this via Nuxt. No issue in vanilla Vue.
+    setInterval(() => {
+      this.$store.dispatch('time/sync')
+    }, 1000 * 10)
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+  @-webkit-keyframes blinker {
+    from { opacity: 1.0; }
+    to { opacity: 0.0; }
+  }
+
+  .blink {
+    -webkit-animation-name: blinker;
+    -webkit-animation-iteration-count: infinite;
+    -webkit-animation-timing-function: cubic-bezier(1.0,0,0,1.0);
+    -webkit-animation-duration: 1s;
+  }
 </style>
