dc9f19f77dd6ff157d5552e2fff49123796cc4c4
Nick Cappadona <nickypasta@gmail.com>
2017-08-23 15:31:39 -0400

    First pass at styles based on mockups
    No layout just yet. Will be using CSS grid. Also letter-spacing will
    need to be adjust per desk. For now, using value based on testing with
    CISER.

--- pages/mann/consult/_desk.vue
+++ pages/mann/consult/_desk.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mann-consult">
-    <h1 class="desk">{{ desk }}</h1>
+    <h1 class="desk" :class="statusClass">{{ desk }}</h1>
 
     <p v-if="description">{{ description }}</p>
 
-    <span class="status">{{ deskInfo.status }}</span> until
-    <time v-html="deskInfo.statusChange"/>
+    <span class="status-current">{{ deskInfo.status }}</span> <span class="until knockout">until</span>
+    <time class="status-change" v-html="deskInfo.statusChange"/>
   </div>
 </template>
 
@@ -43,27 +43,70 @@ export default {
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Lato:300,400,700');
 
+  html {
+    &.status {
+      &--by-appointment {
+        background: #485963;
+      }
+
+      &--closed {
+        background: #b42b5a;
+      }
+
+      &--open {
+        background: #3d9488;
+      }
+    }
+  }
+
+  body {
+    margin: 0;
+  }
+
   .desk {
+    margin: 0;
+    margin-left: -.04em;
+    font-size: 29vh;
+    letter-spacing: 10vw;
+    overflow: hidden;
+    line-height: .9em;
     text-transform: uppercase;
+
+    &.status--by-appointment {
+      background: #657c8a;
+    }
+
+    &.status--closed {
+      background: #d93663;
+    }
+
+    &.status--open {
+      background: #30776b;
+    }
+  }
+
+  .knockout {
+    opacity: .5;
   }
 
   .mann-consult {
+    padding-left: .5em;
     font-family: 'Lato', sans-serif;
   }
 
-  .status {
+  .status-change {
+    display: block;
+    font-size: 14vh;
+  }
+
+  .status-current {
+    font-size: 20vh;
     text-transform: capitalize;
+  }
 
-    &--by-appointment {
-      background: #485963;
-    }
-
-    &--closed {
-      background: #B42B5A;
-    }
-
-    &--open {
-      background: #3D9488;
-    }
+  .until {
+    margin-left: .18em;
+    font-size: 10vh;
+    font-weight: 100;
   }
 </style>
