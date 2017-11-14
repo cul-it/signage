c15eb2f756fbceed4e393d8f765c28b5bc43103b
Nick Cappadona <nickypasta@gmail.com>
2017-11-14 11:05:50 -0500

    Further consistency in template markup
    * two spaces for indenting Lines
    
    * don't indent first line within <template> (vue-loader recommendation)

--- components/oku-circ.vue
+++ components/oku-circ.vue
@@ -4,22 +4,24 @@
     <h1>{{ location }}</h1>
   </header>
 
-  <h2 :class="location + '-laptops'"><i class="fa fa-laptop icon-laptop" aria-hidden="true"></i> Laptops </h2>
+  <h2 :class="location + '-laptops'">
+    <i class="fa fa-laptop icon-laptop" aria-hidden="true"></i> Laptops
+  </h2>
 
   <div :class="location + '-laptops__availability'">
-      <device type="apple" :count="laptops[location].macAvailable"/>
-      <device type="windows" :count="laptops[location].windowsAvailable"/>
+    <device type="apple" :count="laptops[location].macAvailable"/>
+    <device type="windows" :count="laptops[location].windowsAvailable"/>
   </div>
 
   <h2 :class="location + '-phone-chargers'">
-        <i class="fa fa-battery-quarter icon-charger" aria-hidden="true"></i> Phone Chargers
+    <i class="fa fa-battery-quarter icon-charger" aria-hidden="true"></i> Phone Chargers
   </h2>
 
   <div :class="location + '-phone-chargers__availability'">
-      <device type="phoneCharger" model="iPhone 4" :count="phoneChargers[location].iphone4Available"/>
-      <device type="phoneCharger" model="iPhone 5 &amp; up" :count="phoneChargers[location].iphoneAvailable"/>
-      <device type="phoneCharger" model="Micro USB" :count="phoneChargers[location].microUsbAvailable"/>
-      <device type="phoneCharger" model="USB-C":count="phoneChargers[location].usbCAvailable"/>
+    <device type="phoneCharger" model="iPhone 4" :count="phoneChargers[location].iphone4Available"/>
+    <device type="phoneCharger" model="iPhone 5 &amp; up" :count="phoneChargers[location].iphoneAvailable"/>
+    <device type="phoneCharger" model="Micro USB" :count="phoneChargers[location].microUsbAvailable"/>
+    <device type="phoneCharger" model="USB-C":count="phoneChargers[location].usbCAvailable"/>
   </div>
 </div>
 </template>
--- components/oku-device.vue
+++ components/oku-device.vue
@@ -1,18 +1,20 @@
 <template>
-  <p>
-    <span class="device-count" :class="{ unavailable: !Number(count) }">{{ count }}</span>
-    <span v-html="deviceStatus"></span>
-    <span class="available" :class="{ unavailable: !Number(count) }"> available</span>
-  </p>
+<p>
+  <span class="device-count" :class="{ unavailable: !Number(count) }">{{ count }}</span>
+  <span v-html="deviceStatus"></span>
+  <span class="available" :class="{ unavailable: !Number(count) }"> available</span>
+</p>
 </template>
 
 <script>
 export default {
   computed: {
     deviceStatus () {
-      if (this.type === 'apple' || this.type === 'windows') {
+      if (this.type === 'apple') {
         return '<span class="fa fa-' + this.type + ' device-icon" aria-hidden="true"></span>'
-      } else {
+      } else if (this.type === 'windows') {
+        return '<span class="fa fa-' + this.type + ' device-icon" aria-hidden="true"></span>'
+      } else if (this.type === 'phoneCharger') {
         return '<span class="device-type__info--availability">' + this.model + '</span>'
       }
     }
