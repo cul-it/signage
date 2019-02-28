6306f03daa14862670c75ef7e58dbb13dfe9447f
Nick Cappadona <cappadona@users.noreply.github.com>
2019-02-28 00:12:29 -0500

    Fetch reservations for all classrooms
    Via for...of loop which is much more accommodating in respect to
    async/await when compared to forEach.

--- store/spaces.js
+++ store/spaces.js
@@ -20,19 +20,23 @@ export const actions = {
     if (isEmpty(state) || Robin.staleCache(state.updated) || Robin.pastChange(state.statusChange) || Robin.nextDay(state.updated)) {
       // Special handling for spaces managed in R25 (Registrar)
       if (R25.isR25(payload.location, payload.category)) {
-        const requestedSpaces = state
+        const requestedSpaces = Object.values(state)
 
-        // FIXME: Need to fetch reservations for each classroom!
-        let feed = await R25.getReservations(this.$axios, Object.values(requestedSpaces)[0])
+        for (const space of requestedSpaces) {
+          let feed = await R25.getReservations(this.$axios, space)
 
-        var schedule = R25.buildSchedule(feed, state, await Robin.openingTime(this.$axios, payload.location, payload.category), await Robin.closingTime(this.$axios, payload.location, payload.category))
+          let schedule = R25.buildSchedule(feed, state, await Robin.openingTime(this.$axios, payload.location, payload.category), await Robin.closingTime(this.$axios, payload.location, payload.category))
+
+          commit('update', schedule)
+        }
       } else {
         // Otherwise, proceed with default reservation handling via LibCal
         let feed = await Robin.getReservations(this.$axios, payload.location)
 
-        schedule = Robin.buildSchedule(feed, state, await Robin.openingTime(this.$axios, payload.location, payload.category), await Robin.closingTime(this.$axios, payload.location, payload.category))
+        let schedule = Robin.buildSchedule(feed, state, await Robin.openingTime(this.$axios, payload.location, payload.category), await Robin.closingTime(this.$axios, payload.location, payload.category))
+
+        commit('update', schedule)
       }
-      commit('update', schedule)
     }
   },
   primeSpaces: ({ commit }, spaces) => commit('prime', spaces)
