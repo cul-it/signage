71a976188b64a6dae8ad1f39c5c63f8c63903660
Nick Cappadona <cappadona@users.noreply.github.com>
2019-03-14 08:14:14 -0400

    More versatile update mutation for spaces store
    Utilize `Object.assign()` to add any/all new keys to existing store
    instead of explicitly setting the schedule.

--- store/spaces.js
+++ store/spaces.js
@@ -7,7 +7,7 @@ export const state = () => ({
 
 export const mutations = {
   prime: (state, data) => data.forEach(d => (state[d.name] = {'id': d.id, 'capacity': d.capacity, 'name': d.name})),
-  update: (state, data) => Object.keys(data).forEach(d => (state[d].schedule = data[d].schedule))
+  update: (state, data) => Object.keys(data).forEach(d => (Object.assign(state[d], data[d])))
 }
 
 export const actions = {
