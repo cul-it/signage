bc896253e7caff56980094902269322760e425a9
Nick Cappadona <cappadona@users.noreply.github.com>
2019-09-19 07:39:57 -0400

    Documentation power up (#159)
    * Documentation for the people
    
    By the people.
    
    * Quickstart: indicate secrets mgmt via dotenv
    
    Useful info...
    
    * Meaningful title element for default route
    
    AKA the homepage. Been meaning to change this up for quite some time.
    
    * TOC for README (jump menu)
    
    * Bump current lineup to the bottom of README

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'CUL Signage',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
