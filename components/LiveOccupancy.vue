69303fb21ef953113b76fd665612cf169e6d9084
Nick Cappadona <cappadona@users.noreply.github.com>
2020-09-29 09:27:21 -0400

    Init occupancy as data then update via fetch
    Trying this alternative approach since the LiveOccupancy component
    wasn't reacting to current Vuex state for occupancy when switching from
    closed to open.
    
    Still not 100% convinced this is a home run (I noticed a flicker of Max
    Capacity rendering after switching to open) but will test in the wild
    for a couple days.
    
    Great reference for new-ish fetch in Nuxt [1] that helped point me in
    this alternative direction (see note about mutating local data).
    
    [1]: https://nuxtjs.org/blog/understanding-how-fetch-works-in-nuxt-2-12#new-fetch

--- components/LiveOccupancy.vue
+++ components/LiveOccupancy.vue
@@ -18,14 +18,16 @@
 
 <script>
 export default {
+  data () {
+    return {
+      occupancy: {}
+    }
+  },
   computed: {
     iconClass () {
       const keepOut = 'max far fa-times-circle'
       const enter = 'fas fa-sign-in-alt'
       return this.vacancy ? enter : keepOut
-    },
-    occupancy () {
-      return this.$store.state.occupancy
     },
     remaining () {
       return this.occupancy.capacity - this.occupancy.current
@@ -38,6 +40,8 @@ export default {
     await this.$store.dispatch('occupancy/fetch', {
       location: this.$route.params.location
     })
+
+    this.occupancy = this.$store.state.occupancy
   },
   mounted () {
     // Check occupancy every two minutes
